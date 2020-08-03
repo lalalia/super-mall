80d93c1adb488b5cca0f7375eb8547a400a734b1
lalalia <t1351964670@qq.com>
2020-08-04 01:44:44 +0800

    '8/4'

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item,index) in titles" class="tab-control-item"
+             @click="currentIndex = index">
+      <span :class="{active: index === currentIndex}">{{item}}</span>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabControl",
+    props: {
+      titles: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    data() {
+      return{
+        currentIndex: 0
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-control{
+    display: flex;
+    text-align: center;
+    font-size: 15px;
+    height: 40px;
+    line-height: 40px;
+    background-color: #fff;
+
+    /*position: fixed;*/
+    /*right: 0;
+    top: 0;
+    left: 0;
+    z-index: 6;*/
+  }
+
+  .tab-control-item{
+    flex: 1;
+  }
+
+  .tab-control-item span {
+    padding: 3px;
+  }
+
+  .active{
+    color: var(--color-high-text);
+    border-bottom: 3px solid var(--color-high-text);
+  }
+
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,6 +5,40 @@
     </nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <home-recommend-view :recommends="recommends"></home-recommend-view>
+    <home-feature/>
+    <tab-control class="tab-control" :titles="titles"></tab-control>
+
+    <ul>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li><li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li><li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li><li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li><li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li><li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li><li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li><li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+    </ul>
 
   </div>
 </template>
@@ -12,9 +46,10 @@
 <script>
   import HomeSwiper from "./childComps/HomeSwiper";
   import HomeRecommendView from "./childComps/HomeRecommendView";
-
+  import HomeFeature from "./childComps/HomeFeature";
 
   import NavBar from "components/common/nav-bar/NavBar";
+  import TabControl from "components/content/tabControl/TabControl";
 
   import {getHomeMultidata} from "network/home";
 
@@ -23,12 +58,15 @@
     components: {
       NavBar,
       HomeSwiper,
-      HomeRecommendView
+      HomeRecommendView,
+      HomeFeature,
+      TabControl
     },
     data() {
       return {
         banners: [],
-        recommends: []
+        recommends: [],
+        titles: ['流行','新款','精选']
       }
     },
     created() {
@@ -42,8 +80,23 @@
 </script>
 
 <style scoped>
+  #home{
+    padding-top: 44px;
+  }
+
   .home-nav {
     background-color: var(--color-tint);
     color: white;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+
+  .tab-control{
+    position: sticky;
+    top: 43px;
   }
 </style>
